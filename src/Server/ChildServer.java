55fca2c0ce74c172742b23c111ff6fc04d527d1e
chung <sinna94@naver.com>
2017-12-17 21:40:01 +0900

    Revert "디비, 서버 수정"
    This reverts commit 49187e3ef1920fb96549af667b76698b81793a21.

--- src/Server/ChildServer.java
+++ src/Server/ChildServer.java
@@ -4,35 +4,33 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import DTO.Account;
 import DTO.Packet;
 
 public class ChildServer extends Thread {
 	private Socket socket = null;
 	private ObjectOutputStream oos = null;
 	private ObjectInputStream ois = null;
-	private DAO dao = null;
 	private Packet packet;
+	protected DAO dao;
 	
-	public ChildServer(Socket socket) throws IOException {
+	public ChildServer(Socket socket, DAO dao) throws IOException {
 		this.socket = socket;
 		this.oos = new ObjectOutputStream(socket.getOutputStream());
 		this.ois = new ObjectInputStream(socket.getInputStream());
-		this.dao = new DAO();
+		this.packet = new Packet();
+		this.dao = dao;
 	}
 
 	@Override
 	public void run() {
 		try {
-			Packet packet;
 			while (true) {
 				packet = (Packet) ois.readObject(); // 패킷 받기
 				switch (packet.getCode()) { // 패킷 해석
 				case "REQ_LOGIN":			// 로그인 요청
-					checkLogin((Account) packet.getData());
+					checkLogin(packet);
 					break;
 
 				default:
@@ -50,38 +48,21 @@ public class ChildServer extends Thread {
 		}
 	}
 
-	public void checkLogin(Account account) throws SQLException{
-		boolean result = dao.checkLoginDB(account);
-
-		if (result == true) { // DB 체크 결과
-			packet.setCode("LOGIN_SUC");
-		} 
-		else {
-			packet.setCode("LOGIN_FAIL");
-		}
-		sendPacket(packet);
-	}
-	
-	public void regist(Account account) throws SQLException{
-		int result = dao.registDB(account);
+	public void checkLogin(Packet packet) throws SQLException, IOException {
+		boolean result = dao.checkLoginDB(packet);
 		
-		if (result == 0 ){
-			packet.setCode("REGI_FAIL");
+		if(result == true){ // DB 체크 결과
+			packet.setCode("LOGIN_SUC");
 		}
 		else{
-			packet.setCode("REGI_SUCC");
+			packet.setCode("LOGIN_FAIL");
 		}
-		
-		sendPacket(packet);
+		oos.writeObject(packet);
 	}
 
-	public void sendPacket(Packet packet) {
-		try {
-			oos.writeObject(packet);
-			oos.flush();
-			oos.reset();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void sendPacket(Packet packet) throws IOException {
+		oos.writeObject(packet);
+		oos.flush();
+		oos.reset();
 	}
 }
